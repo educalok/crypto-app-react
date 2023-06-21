ba3c3dcbf5977d8847f5d2426d55a66d5ab311f1
Edu <eduardo.calzone@gmail.com>
2023-06-21 22:45:28 +0200

    Warning fixed

--- src/App.js
+++ src/App.js
@@ -8,13 +8,12 @@ import Navbar from './components/Navbar'
 function App () {
   const [coins, setCoins] = useState([])
 
-  const url =
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
-
   useEffect(() => {
-    const getCryptosValues = () => {
+    const getCryptosValues = () =>
       axios
-        .get(url)
+        .get(
+          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
+        )
         .then(response => {
           setCoins(response.data)
           // console.log(response.data[0])
@@ -22,9 +21,8 @@ function App () {
         .catch(error => {
           console.log(error)
         })
-    }
     getCryptosValues()
-  }, [url])
+  }, [])
 
   return (
     <>
